c5c6edfb6ec45449abbb26edf05bc8abe4084141
danasselin <dasselin86@gmail.com>
2016-11-12 22:49:23 -0500

    Initial passing solution for sudoku board status

--- doneornot.js
+++ doneornot.js
@@ -26,14 +26,10 @@ https://www.codewars.com/kata/53db96041f1a7d32dc0004d2/train/javascript
 */
 
 const doneOrNot = (board) => {
-  for(let i = 0; i < board.length; i++){
-    if (containsAll(getRow(board, i)) &&
-    containsAll(getColumn(board, i)) &&
-    containsAll(getRegion( board, i, i !== 0 ? i + 3 : i ))) {
-      return "Finished!";
-    } else {
-      return "Try again!";
-    }
+  if(checkRows(board) && checkColumns(board) && checkRegions(board)){
+    return "Finished!";
+  } else {
+    return "Try again!";
   }
 }
 
@@ -45,19 +41,16 @@ const containsAll = (array) => {
 }
 
 const getRow = (board, i) => {
-  console.log( board[i] );
   return board[i];
 }
 
 const getColumn = (board, i) => {
   const column = board.map((el) => el[i]);
-  console.log( column );
   return column;
 }
 
 const getRegion = (board, x, y) => {
   const region = new Array();
-
   for(let i = 0; i < 3; i++){
     let currentIndex = y;
     for(let w = 0; w < 3; w++){
@@ -66,28 +59,31 @@ const getRegion = (board, x, y) => {
     }
     x += 1;
   }
-  console.log( region );
   return region;
 }
 
-const board1 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
-               [6, 7, 2, 1, 9, 5, 3, 4, 8],
-               [1, 9, 8, 3, 4, 2, 5, 6, 7],
-               [8, 5, 9, 7, 6, 1, 4, 2, 3],
-               [4, 2, 6, 8, 5, 3, 7, 9, 1],
-               [7, 1, 3, 9, 2, 4, 8, 5, 6],
-               [9, 6, 1, 5, 3, 7, 2, 8, 4],
-               [2, 8, 7, 4, 1, 9, 6, 3, 5],
-               [3, 4, 5, 2, 8, 6, 1, 7, 9]]
+const checkRows = (board) => {
+  for(let i = 0; i < board.length; i++){
+    if (!containsAll(getRow(board, i))){ return false };
+  }
+  return true;
+}
 
-const board2 = [[5, 3, 4, 6, 7, 8, 9, 1, 2],
-               [6, 7, 2, 1, 9, 0, 3, 4, 9],
-               [1, 0, 0, 3, 4, 2, 5, 6, 0],
-               [8, 5, 9, 7, 6, 1, 0, 2, 0],
-               [4, 2, 6, 8, 5, 3, 7, 9, 1],
-               [7, 1, 3, 9, 2, 4, 8, 5, 6],
-               [9, 0, 1, 5, 3, 7, 2, 1, 4],
-               [2, 8, 7, 4, 1, 9, 6, 3, 5],
-               [3, 0, 0, 4, 8, 1, 1, 7, 9]]
+const checkColumns = (board) => {
+  for(let i = 0; i < board.length; i++){
+    if (!containsAll(getColumn(board, i))){ return false };
+  }
+  return true;
+}
 
-console.log( doneOrNot(board1) );
+const checkRegions = (board) => {
+  for(let x = 0; x < board.length; x+=3){
+    let y = 0;
+    if(!containsAll(getRegion(board, x, y)) ||
+       !containsAll(getRegion(board, x, y += 3)) ||
+       !containsAll(getRegion(board, x, y += 3))) {
+         return false;
+    }
+  }
+  return true;
+}
